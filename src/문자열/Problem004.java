314ce412527c4e25557f175c62712e0e642168a8
itsiyeon <itsiyeon@nate.com>
2024-08-29 21:38:21 +0900

    4. 단어 뒤집기

---
+++ src/문자열/Problem004.java
@@ -0,0 +1,22 @@
+package 문자열;
+
+import java.util.Scanner;
+
+public class Problem004 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s = scanner.next();
+            System.out.println(solution(s));
+        }
+    }
+
+    public static String solution(String str) {
+        String answer = "";
+        for (int i = str.length(); i > 0; i--) {
+            answer += String.valueOf(str.charAt(i-1));
+        }
+        return answer;
+    }
+}
