fc8f81917577a0b62a0d608b7d2586c175670020
itsiyeon <itsiyeon@nate.com>
2026-01-30 23:18:43 +0900

    7. 조합의 경우수(메모이제이션)

---
+++ src/dfs/Problem005.java
@@ -0,0 +1,37 @@
+package dfs;
+
+import java.util.Scanner;
+
+public class Problem005 {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int r = kb.nextInt();
+		int[][] dp = new int[n+1][r+1];
+//		System.out.println(solution(n, r));
+		System.out.println(solution2(n, r, dp));
+	}
+
+	public static int dfs(int n, int r) {
+		if (r == 0 || n == r) return 1;
+		return dfs(n - 1, r - 1) + dfs(n - 1, r);
+	}
+
+	public static int solution(int n, int r) {
+		int l = 0;
+		return dfs(n, r);
+	}
+
+	public static int solution2(int n, int r, int[][] dp) {
+		return memo(n, r, dp);
+	}
+
+	public static int memo(int n, int r, int[][] dp) {
+		if (r == 0 || n == r) return 1;
+		if (dp[n][r] != 0) return dp[n][r];
+		else {
+			dp[n][r] = memo(n - 1, r - 1, dp) + memo(n - 1, r, dp);
+		}
+		return dp[n][r];
+	}
+}
